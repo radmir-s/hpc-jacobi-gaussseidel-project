ea6ae7dbd30cb4a3a7741a35220723e80ec6a956
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-12-08 14:54:52 -0600

    fixed i=j issue

--- gauss-seidel-omp.c
+++ gauss-seidel-omp.c
@@ -104,7 +104,7 @@ int main()
 				{
 					sum += A[i][j]*x1[j];
 				}
-				else
+				else if (j != i)
 				{
 					sum += A[i][j]*x0[j];
 				}
@@ -144,7 +144,7 @@ int main()
 	// end timer
 	end = clock();
 	cpu_time_used = 1000*((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Time = %lf ms\nOuter1 = %d\nInner1 = %d\nLoop = %d\nOuter2 = %d\nInner2 = %d", cpu_time_used, INNER1, LOOP, OUTER2, INNER2);
+	printf("Time = %lf ms\nOuter1 = %d\nInner1 = %d\nLoop = %d\nOuter2 = %d\nInner2 = %d\n", cpu_time_used, INNER1, LOOP, OUTER2, INNER2);
 
 
 	for (size_t i = 0; i < 10; i++)
