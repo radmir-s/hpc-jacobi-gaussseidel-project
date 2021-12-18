ff3c50c1a2068ca1dcb361be49ba0cc93bc7b0e6
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-12-17 19:19:34 -0600

    small change

--- job-jacobi-tau.sh
+++ job-jacobi-tau.sh
@@ -4,9 +4,9 @@
 #SBATCH -t 03:00:00
 #SBATCH -n 48
 
-module load tau/2.27.2
-export TAU_MAKEFILE=$TAU/Makefile.tau-intelomp-icpc-papi-ompt-pdt-openmp
-export TAU_METRICS="PAPI_L1_TCM"
+#module load tau/2.27.2
+#export TAU_MAKEFILE=$TAU/Makefile.tau-intelomp-icpc-papi-ompt-pdt-openmp
+#export TAU_METRICS="PAPI_L1_TCM"
 
 for size in  64 4096 262144; do
 for inner in 1; do
@@ -15,14 +15,14 @@ for loop in 1; do
 
 if [ $inner != 12 -o $outer != 12 ]; then
 
-tau_cc.sh -lm -fopenmp -DOUTER=$outer -DINNER=$inner  \
+gcc.sh -lm -fopenmp -DOUTER=$outer -DINNER=$inner  \
 -DLOOP=$loop -DN=$size \
 jacobi-omp-nested.c -o jacobi-omp-nested.exe
 
 ./jacobi-omp-nested.exe > results/jacobi-$size-$inner-$outer-$loop.txt
 
-export TAU_METRICS="PAPI_FLOPS"
-pprof > results/jacobi-l1-$size-$inner-$outer-$loop.txt
+#export TAU_METRICS="PAPI_FLOPS"
+#pprof > results/jacobi-l1-$size-$inner-$outer-$loop.txt
 
 
 fi
