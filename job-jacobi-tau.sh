b1b5eba11d7e42a7afb2b76c3272e61884241c40
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-12-17 19:06:01 -0600

    last run

--- job-jacobi-tau.sh
+++ job-jacobi-tau.sh
@@ -8,10 +8,10 @@ module load tau/2.27.2
 export TAU_MAKEFILE=$TAU/Makefile.tau-intelomp-icpc-papi-ompt-pdt-openmp
 export TAU_METRICS="PAPI_L1_TCM"
 
-for size in 32 128 512 2048 8192; do
-for inner in 1 2 4 8 12; do
-for outer in 1 2 4 8 12; do
-for loop in 1 2 4 8; do
+for size in  64 4096 262144; do
+for inner in 1; do
+for outer in 1 8 32 96; do
+for loop in 1; do
 
 if [ $inner != 12 -o $outer != 12 ]; then
 
@@ -21,14 +21,9 @@ jacobi-omp-nested.c -o jacobi-omp-nested.exe
 
 ./jacobi-omp-nested.exe > results/jacobi-$size-$inner-$outer-$loop.txt
 
-export TAU_METRICS="PAPI_L1_TCM"
+export TAU_METRICS="PAPI_FLOPS"
 pprof > results/jacobi-l1-$size-$inner-$outer-$loop.txt
 
-export TAU_METRICS="PAPI_L2_TCM"
-pprof > results/jacobi-l2-$size-$inner-$outer-$loop.txt
-
-export TAU_METRICS="PAPI_L3_TCM"
-pprof > results/jacobi-l3-$size-$inner-$outer-$loop.txt
 
 fi
 
